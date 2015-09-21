838e763f9a8283828c61b45e290a3c274fc5a1bb
albert925 <albertarias925@gmail.com>
2015-09-21 16:15:13 -0500

    bd-pars

--- express/index.js
+++ express/index.js
@@ -1,9 +1,31 @@
 var express=require("express");
+var bodyParser=require("body-parser");
 var app=express();
 app.set("view engine","jade");
 app.use(express.static("public"));//carpetas estatica assets carpeta creada public
-app.get("/",function (solictud,respuesta) {//(peticoin,respuesta)
-	respuesta.render("index");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true }));
+
+//convertir cada campo en tipo de campo
+/*var formiSchema={
+	name:String
+};*/
+
+app.get("/",function (solictud,respuesta) {//(peticoin,respuesta)  /nombre que qiueres colocar la ruta '/ olo dominio o /nombrequequierascolocar'
+	respuesta.render("index"); //index
 });
+app.get("/nosotros",function (solictud,respuesta) {
+	respuesta.render("nosotros/new");
+});
+app.get("/form",function (solictud,respuesta) {
+	respuesta.render("formpost");
+});
+app.post("/form/sl",function (slic,res) {
+	//optiniendo los datos en post
+	//var data={name:slic.body.tit};
+	console.log(slic.body);
+	res.render("formpost");
+})
 app.listen(5000);
 console.log("Puerto 5000");
